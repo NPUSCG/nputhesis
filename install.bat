5addd4279e96f30a7ceaec13afe6c8544c7177bb
Yang Zongze <yangzongze@gmail.com>
2020-02-18 23:21:31 +0800

    Move sample to dtx file.

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
-del nputhesis.cls nputhesis.pdf
+del nputhesis.cls nputhesis.pdf nputhesis-sample.tex nputhesis-sample.pdf 
 latex nputhesis.ins
 xelatex nputhesis.dtx
 makeindex -s gglo.ist -o nputhesis.gls nputhesis.glo
 makeindex -s gind.ist -o nputhesis.ind nputhesis.idx
 xelatex nputhesis.dtx
 del nputhesis.gls nputhesis.glo nputhesis.ind nputhesis.idx nputhesis.ilg nputhesis.aux nputhesis.log nputhesis.toc
+latexmk -xelatex nputhesis-sample.tex
+latexmk -c
