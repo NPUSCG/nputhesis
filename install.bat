edc855a5be24e95ab59c3931e5e76ab317af6aef
Yang Zongze <yangzongze@gmail.com>
2020-02-19 17:14:15 +0800

    Move almost all file into dtx

--- install.bat
+++ install.bat
@@ -1,10 +1,5 @@
-del nputhesis.cls nputhesis.pdf nputhesis-sample.tex nputhesis-sample.pdf 
-latex nputhesis.ins
-xelatex nputhesis.dtx
-makeindex -s gglo.ist -o nputhesis.gls nputhesis.glo
-makeindex -s gind.ist -o nputhesis.ind nputhesis.idx
-xelatex nputhesis.dtx
-xelatex nputhesis.dtx
-del nputhesis.gls nputhesis.glo nputhesis.ind nputhesis.idx nputhesis.ilg nputhesis.aux nputhesis.log nputhesis.toc
-latexmk -xelatex nputhesis-sample.tex
-latexmk -c
+latexmk -xelatex -synctex=1 nputhesis.dtx -r .dtxrc
+latexmk -c nputhesis.dtx -r .dtxrc
+latexmk -xelatex -synctex=1 nputhesis-sample.tex -r latexmkrc
+latexmk -c nputhesis-sample.tex -r latexmkrc
+pause
