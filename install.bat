6c16c9a98954b367e7b3f23da6c67608fd2583f6
Yang Zongze <yangzongze@gmail.com>
2020-02-19 13:45:13 +0800

    change ldxdoc to ctxdoc

--- install.bat
+++ install.bat
@@ -4,6 +4,7 @@ xelatex nputhesis.dtx
 makeindex -s gglo.ist -o nputhesis.gls nputhesis.glo
 makeindex -s gind.ist -o nputhesis.ind nputhesis.idx
 xelatex nputhesis.dtx
+xelatex nputhesis.dtx
 del nputhesis.gls nputhesis.glo nputhesis.ind nputhesis.idx nputhesis.ilg nputhesis.aux nputhesis.log nputhesis.toc
 latexmk -xelatex nputhesis-sample.tex
 latexmk -c
